d311836b61f7585ecdce560f67c6f55d024cca00
fotistsiou <fotis.tsioumas@outlook.com>
2024-10-23 10:17:40 +0300

    Chinese Remainder Theorem

---
+++ src/fotistsiou/java/algorithms/ChineseRemainderTheorem/ChineseRemainderTheorem.java
@@ -0,0 +1,21 @@
+package fotistsiou.java.algorithms.ChineseRemainderTheorem;
+
+import java.util.Scanner;
+
+public class ChineseRemainderTheorem {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+        scanner.close();
+
+        // Use the Chinese Remainder Theorem to calculate the age
+        // Formula: (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println("Your age is " + age + ".");
+    }
+}
